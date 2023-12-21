fde6c4a8698eb4b50fe4da2f7dacfdf770c28102
salt431 <35209044+salt431@users.noreply.github.com>
2023-12-21 00:21:42 -0500

    Update main.rs

--- main.rs
+++ main.rs
@@ -1,147 +1,59 @@
-use rand::Rng;
-
-struct ConvLayer {
+use std::f64;
+use rand::*;
+struct Neuron {
     weights: Vec<f64>,
-    biases: Vec<f64>,
+    bias: f64,
 }
 
-impl ConvLayer {
-    fn new(input_size: usize, kernel_size: usize) -> Self {
+impl Clone for Neuron {
+    fn clone(&self) -> Self {
+        let mut new_weights = self.weights.clone();
         let mut rng = rand::thread_rng();
-        ConvLayer {
-            weights: (0..input_size - kernel_size + 1).map(|_| rng.gen()).collect(),
-            biases: vec![rng.gen(); kernel_size],
+        for weight in &mut new_weights {
+            *weight = rng.gen::<f64>();
         }
-    }
 
-    fn forward(&self, input: &[f64]) -> Vec<f64> {
-        let mut output = vec![0.; self.biases.len()];
-        for i in 0..input.len() - self.weights.len() + 1 {
-            let mut sum = 0.0;
-            for j in 0..self.weights.len() {
-                sum += self.weights[j] * input[i + j];
-            }
-            output[i / (input.len() - kernel_size + 1)] = sum;
-        }
-        output
-    }
-
-    fn backprop(&mut self, errors: &[f64], input: &[f64]) {
-        for i in 0..self.weights.len() {
-            self.weights[i] -= self.learning_rate * errors[i / (input.len() - kernel_size + 1)] * input[i];
-        }
-        for j in 0..self.biases.len() {
-            self.biases[j] -= self.learning_rate * errors[j] as f64;
+        Neuron {
+            weights: new_weights,
+            bias: self.bias.clone(),
         }
     }
 }
 
-struct CNN {
-    layers: Vec<Arc<Mutex<ConvLayer>>>,
-}
-
-impl CNN {
-    fn new(input_size: usize, kernel_sizes: &[usize]) -> Self {
-        let mut layers = vec![];
-        for kernel_size in kernel_sizes {
-            layers.push(Arc::new(Mutex::new(ConvLayer::new(input_size, *kernel_size))));
-            input_size *= 2;
-        }
-        CNN { layers }
-    }
-
-    fn forward(&self, input: &[f64]) -> Vec<f64> {
-        let mut output = vec![0.; self.layers[0].lock().unwrap().biases.len()];
-        for layer in self.layers.iter().skip(1) {
-            let prev_output = self.forward_prev_layer(&self.layers[..layer.as_ref().drwaw().index()]);
-            let conv_layer = layer.lock().unwrap();
-            output = prev_output.iter().map(|&x| x).zip(conv_layer.forward(input)).collect();
-        }
-        self.layers[0].lock().unwrap().forward(input)
-    }
-
-    fn backprop(&mut self, errors: &[f64], input: &[f64]) {
-        let mut last_layer = self.layers.last_mut().unwrap();
-        last_layer.backprop(&errors, &input);
-        for layer in (self.layers.iter()).rev() {
-            if let Some(conv_layer) = layer.downcast_ref::<ConvLayer>() {
-                conv_layer.backprop(&errors, &last_layer.lock().unwrap().forward(input));
-            }
-        }
-    }
-
-    fn forward_prev_layer(&self, layers: &[Arc<Mutex<ConvLayer>>]) -> Vec<f64> {
-        let mut output = vec![0.; self.layers[0].lock().unwrap().biases.len()];
-        for layer in layers.iter() {
-            let conv_layer = layer.lock().unwrap();
-            let prev_output = if layers.len() == 1 { input } else { output.clone() };
-            output = prev_output.iter().map(|&x| x).zip(conv_layer.forward(&prev_output)).collect();
-        }
-        output
-    }
-}
-
-struct ImageNetDataset {
-    images: Vec<Arc<Image>>,
-    labels: Vec<f64>,
-}
-
-impl ImageNetDataset {
-    fn new(images: Vec<Arc<Image>>, labels: Vec<f64>) -> Self {
-        ImageNetDataset { images, labels }
-    }
-
-    fn random_split(&self, split: f64) -> (ImageNetDataset, ImageNetDataset) {
+impl Neuron {
+    fn new(num_inputs: usize) -> Self {
+        let mut weights = vec![0.0; num_inputs];
         let mut rng = rand::thread_rng();
-        let len = self.images.len();
-        let split_idx = rng.gen::<usize>((0..len).into());
-        (
-            ImageNetDataset {
-                images: self.images[..split_idx].to_vec(),
-                labels: self.labels[..split_idx].to_vec()
-            },
-            ImageNetDataset {
-                images: self.images[split_idx..].to_vec(),
-                labels: self.labels[split_idx..].to_vec()
-            }
-        )
-    }
-}
+        for weight in &mut weights {
+            *weight = rng.gen::<f64>();
+        }
 
-struct CNNTrainer {
-    model: Arc<Mutex<CNN>>,
-    dataset: ImageNetDataset,
-    learning_rate: f64,
-}
-
-impl CNNTrainer {
-    fn new(model: Arc<Mutex<CNN>>, dataset: ImageNetDataset, learning_rate: f64) -> Self {
-        CNNTrainer { model, dataset, learning_rate }
-    }
-
-    fn train(&mut self, epochs: u32) {
-        for _ in 0..epochs {
-            let (images, labels) = self.dataset.random_split(0.8);
-            self.train_epoch(images, labels);
-            println!("Epoch {}/{} completed!", _ + 1, epochs);
+        Neuron {
+            weights,
+            bias: 0.0,
         }
     }
 
-    fn train_epoch(&mut self, images: Vec<Arc<Image>>, labels: Vec<f64>) {
-        let mut data = vec![(); images.len() * 32].map(|_| 0.0 as f64);
-        for (image, label) in images.iter().zip(labels.iter()) {
-            let pixels = image.pixels().to_vec();
-            data[..32].clone_from_slice(&pixels);
-            let output = self.model.lock().unwrap().forward(&data);
-            let error = label - output;
-            self.model.lock().unwrap().backprop(&vec![error], &pixels);
+    fn forward(&self, inputs: Vec<f64>) -> f64 {
+        let mut output = self.weights[0] * inputs[0] + self.bias;
+        for i in 1..self.weights.len() {
+            output += self.weights[i] * inputs[i];
         }
+
+        output
     }
 }
 
 fn main() {
-    let (images, labels) = ImageNetDataset::new(Arc::new(Image::new()));
-    let model = Arc::new(Mutex::new(CNN::new()));
-    let trainer = CNNTrainer::new(model.clone(), images.clone(), 0.1);
-    trainer.train(5);
+    let num_inputs = 7;
+    let mut neurons = vec![Neuron::new(num_inputs); 24];
+
+    // Sample input data
+    let inputs = vec![0.1, 0.2, 0.3, 0.4, 1.1, 1.2, 1.3];
+    let target = 1.0;
+
+    for neuron in &mut neurons {
+        let output = neuron.forward(inputs.clone());
+        println!("Neuron Output: {}", output);
+    }
 }
